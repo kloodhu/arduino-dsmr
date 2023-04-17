71bd25fa71afea500bd7d8e50fbe984619846538
kloodhu <59362703+kloodhu@users.noreply.github.com>
2023-04-17 12:30:59 +0200

    Update fields.cpp
    deleting commented lines

--- src/dsmr/fields.cpp
+++ src/dsmr/fields.cpp
@@ -56,13 +56,6 @@ constexpr char units::kvarh[];
 constexpr ObisId identification::id;
 constexpr char identification::name[];
 
-/*constexpr ObisId p1_version::id;*/
-/*constexpr char p1_version::name[];*/
-
-/* extra field for Belgium */
-/*constexpr ObisId p1_version_be::id;*/
-/*constexpr char p1_version_be::name[];*/
-
 constexpr ObisId timestamp::id;
 constexpr char timestamp::name[];
 
@@ -97,14 +90,6 @@ constexpr char total_imported_energy::name[];
 constexpr ObisId total_exported_energy::id;
 constexpr char total_exported_energy::name[];
 
-/* extra for Lux */
-/*constexpr ObisId reactive_power_delivered::id;*/
-/*constexpr char reactive_power_delivered::name[];*/
-
-/* extra for Lux */
-/*constexpr ObisId reactive_power_returned::id;*/
-/*constexpr char reactive_power_returned::name[];*/
-
 constexpr ObisId electricity_tariff::id;
 constexpr char electricity_tariff::name[];
 
@@ -116,39 +101,6 @@ constexpr char power_returned::name[];
 
 constexpr ObisId electricity_threshold::id;
 constexpr char electricity_threshold::name[];
-
-/*constexpr ObisId electricity_switch_position::id;*/
-/*constexpr char electricity_switch_position::name[];*/
-
-/*constexpr ObisId electricity_failures::id;*/
-/*constexpr char electricity_failures::name[];*/
-
-/*constexpr ObisId electricity_long_failures::id;*/
-/*constexpr char electricity_long_failures::name[];*/
-
-/*constexpr ObisId electricity_failure_log::id;*/
-/*constexpr char electricity_failure_log::name[];*/
-
-/*constexpr ObisId electricity_sags_l1::id;*/
-/*constexpr char electricity_sags_l1::name[];*/
-
-/*constexpr ObisId electricity_sags_l2::id;*/
-/*constexpr char electricity_sags_l2::name[];*/
-
-/*constexpr ObisId electricity_sags_l3::id;*/
-/*constexpr char electricity_sags_l3::name[];*/
-
-/*constexpr ObisId electricity_swells_l1::id;*/
-/*constexpr char electricity_swells_l1::name[];*/
-
-/*constexpr ObisId electricity_swells_l2::id;*/
-/*constexpr char electricity_swells_l2::name[];*/
-
-/*constexpr ObisId electricity_swells_l3::id;*/
-/*constexpr char electricity_swells_l3::name[];*/
-
-/*constexpr ObisId message_short::id;*/
-/*constexpr char message_short::name[];*/
 
 constexpr ObisId message_long::id;
 constexpr char message_long::name[];
@@ -170,102 +122,3 @@ constexpr char current_l2::name[];
 
 constexpr ObisId current_l3::id;
 constexpr char current_l3::name[];
-
-/*constexpr ObisId power_delivered_l1::id;
-constexpr char power_delivered_l1::name[];
-
-constexpr ObisId power_delivered_l2::id;
-constexpr char power_delivered_l2::name[];
-
-constexpr ObisId power_delivered_l3::id;
-constexpr char power_delivered_l3::name[];
-
-constexpr ObisId power_returned_l1::id;
-constexpr char power_returned_l1::name[];
-
-constexpr ObisId power_returned_l2::id;
-constexpr char power_returned_l2::name[];
-
-constexpr ObisId power_returned_l3::id;
-constexpr char power_returned_l3::name[];*/
-
-/* LUX */
-/*constexpr ObisId reactive_power_delivered_l1::id;
-constexpr char reactive_power_delivered_l1::name[];
-
-constexpr ObisId reactive_power_delivered_l2::id;
-constexpr char reactive_power_delivered_l2::name[];
-
-constexpr ObisId reactive_power_delivered_l3::id;
-constexpr char reactive_power_delivered_l3::name[];
-
-constexpr ObisId reactive_power_returned_l1::id;
-constexpr char reactive_power_returned_l1::name[];
-
-constexpr ObisId reactive_power_returned_l2::id;
-constexpr char reactive_power_returned_l2::name[];
-
-constexpr ObisId reactive_power_returned_l3::id;
-constexpr char reactive_power_returned_l3::name[];*/
-
-/*constexpr ObisId gas_device_type::id;
-constexpr char gas_device_type::name[];
-
-constexpr ObisId gas_equipment_id::id;
-constexpr char gas_equipment_id::name[];
-
-constexpr ObisId gas_valve_position::id;
-constexpr char gas_valve_position::name[];*/
-
-/* _NL */
-/*constexpr ObisId gas_delivered::id;
-constexpr char gas_delivered::name[];*/
-
-/* _BE */
-/*constexpr ObisId gas_delivered_be::id;
-constexpr char gas_delivered_be::name[];
-
-constexpr ObisId gas_delivered_text::id;
-constexpr char gas_delivered_text::name[];
-
-constexpr ObisId thermal_device_type::id;
-constexpr char thermal_device_type::name[];
-
-constexpr ObisId thermal_equipment_id::id;
-constexpr char thermal_equipment_id::name[];
-
-constexpr ObisId thermal_valve_position::id;
-constexpr char thermal_valve_position::name[];
-
-constexpr ObisId thermal_delivered::id;
-constexpr char thermal_delivered::name[];
-
-constexpr ObisId water_device_type::id;
-constexpr char water_device_type::name[];
-
-constexpr ObisId water_equipment_id::id;
-constexpr char water_equipment_id::name[];
-
-constexpr ObisId water_valve_position::id;
-constexpr char water_valve_position::name[];
-
-constexpr ObisId water_delivered::id;
-constexpr char water_delivered::name[];
-
-constexpr ObisId sub_device_type::id;
-constexpr char sub_device_type::name[];
-
-constexpr ObisId sub_equipment_id::id;
-constexpr char sub_equipment_id::name[];
-
-constexpr ObisId sub_valve_position::id;
-constexpr char sub_valve_position::name[];
-
-constexpr ObisId sub_delivered::id;
-constexpr char sub_delivered::name[];*/
-
-/*constexpr ObisId active_energy_import_current_average_demand::id;
-constexpr char active_energy_import_current_average_demand::name[];
-
-constexpr ObisId active_energy_import_maximum_demand_running_month::id;
-constexpr char active_energy_import_maximum_demand_running_month::name[];*/
